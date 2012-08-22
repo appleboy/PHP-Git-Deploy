8d49dad6c757e5bf9b70fb1eba63a8a2dd6fbd74
Bo-Yi Wu <appleboy.tw@gmail.com>
2012-08-22 17:30:06 +0800

    add document on github config
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- config/github.php
+++ config/github.php
@@ -12,12 +12,44 @@
 /**
  * github config
  *
- * git command path
+ * 1. git command absolute path
+ * 2. project name, path and branch name
+ *
+ * Format:
+ * array(
+ *     'project_name' => array(
+ *         'branch_name' => array('base_path' => 'folder_path')
+ *     )
+ * );
+ *
+ * If your github project url path:
+ * https://github.com/appleboy/CodeIgniter-Git-Deploy
+ *
+ * For example: Single project, multi branch
+ * array(
+ *     'codeigniter-git-deploy' => array(
+ *         'master' => array('base_path' => '/path/CodeIgniter-Git-Deploy_1/'),
+ *         'develop' => array('base_path' => '/path/CodeIgniter-Git-Deploy_2/')
+ *     )
+ * );
+ *
+ * For example: Multi project, multi branch
+ * array(
+ *     'codeigniter-git-deploy' => array(
+ *         'master' => array('base_path' => '/path/CodeIgniter-Git-Deploy_1/'),
+ *         'develop' => array('base_path' => '/path/CodeIgniter-Git-Deploy_2/')
+ *     ),
+ *     'codeigniter-my-model' => array(
+ *         'master' => array('base_path' => '/path/CodeIgniter-MY-Model_1/'),
+ *         'develop' => array('base_path' => '/path/CodeIgniter-MY-Model_2/')
+ *     )
+ * );
+ *
  */
 
 $config['git_path'] = '/usr/local/bin/git';
 $config['github'] = array(
-    'shell-script' => array('master' => array('base_path' => '/usr/home/git/Shell-Script/')),
-    'config'       => array('master' => array('base_path' => '/home/git/config/')),
-    'codeIgniter-git-deploy' => array('master' => array('base_path' => '/home/git/test/CodeIgniter-Git-Deploy/')),
+    'codeigniter-git-deploy' => array(
+        'master' => array('base_path' => '/home/git/test/CodeIgniter-Git-Deploy/')
+    )
 );

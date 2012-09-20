2a6ea81bdd3cac9b45e82b5fe1f14b42d28d102e
Bo-Yi Wu <appleboy.tw@gmail.com>
2012-09-20 09:41:03 +0800

    update test
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- tests/DeployTest.php
+++ tests/DeployTest.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once('deploy');
+
 class DeployTest extends PHPUnit_Framework_TestCase
 {
     public function testHelloWorld()
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 
+// Errors on full!
+ini_set('display_errors', 1);
+error_reporting(E_ALL | E_STRICT);
+$dir = realpath(dirname(__FILE__));
+
 function loader($class)
 {
     $file = $class . '.php';

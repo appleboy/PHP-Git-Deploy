b59938768ec05ce84cb0b7854bd33944d6cb7062
Bo-Yi Wu <appleboy.tw@gmail.com>
2012-09-20 10:45:54 +0800

    modified unit test
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- tests/DeployTest.php
+++ tests/DeployTest.php
@@ -1,8 +1,7 @@
 <?php
 
-include('deploy');
-
 namespace Web\Test;
+use Web\Deploy;
 
 class DeployTest extends \PHPUnit_Framework_TestCase
 {
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,15 +3,9 @@
 // Errors on full!
 ini_set('display_errors', 1);
 error_reporting(E_ALL | E_STRICT);
-$dir = realpath(dirname(__FILE__));
 
-function loader($class)
-{
-    $file = '.src/' . $class . '.php';
-    echo $file . ' xxxxxxxxxxxxxxx'; 
-    if (file_exists($file)) {
-        require $file;
-    }
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+    die('You must set up the project dependencies, run the following commands:'.PHP_EOL.
+        'curl -s http://getcomposer.org/installer | php'.PHP_EOL.
+        'php composer.phar install'.PHP_EOL);
 }
-
-spl_autoload_register('loader');
